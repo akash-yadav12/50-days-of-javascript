768f852cca1ccf2082b25d8d0ddc8df134f22a76
Riam28 <riya.mt@somaiya.edu>
2021-12-21 13:41:23 +0530

    Day 9

---
+++ Day9.js
@@ -0,0 +1,11 @@
+const obj = {};
+
+function isEmpty(obj) {
+    // write your solution here
+    if(Object.keys(obj).length === 0){
+        return true
+    }
+    return false
+}
+
+console.log(`is empty object: ${isEmpty(obj)}`)
