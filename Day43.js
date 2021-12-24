aabb5f9aa9aea06c072259c68bb7bbc881b85ad2
akash-yadav12 <akash.ry@somaiya.edu>
2021-12-24 19:24:30 +0530

    Day43

---
+++ Day43.js
@@ -0,0 +1,19 @@
+function high(x) {
+  //code your magic here
+  mp = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26}
+  let arr = x.split(' ')
+  let res
+  let mx = 0
+  let total = 0
+  for(let el of arr){
+      total = 0
+      for(let ch of el){
+          total += mp[ch]
+      }
+      if(total>mx){
+          res = el
+          mx = total
+      }
+  }
+  return res
+}
